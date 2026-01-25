66d2d100a01fc2295d7b428a5540a05d97b9ed37
Nishant Kumar <nishant.kumaar002@gmail.com>
2026-01-25 16:31:43 +0530

    25-01-2026 04:31

---
+++ src/PrintAndPrintln.kt
@@ -0,0 +1,18 @@
+fun main(){
+    print("Hi my name is ")
+    print("Nishant Kumar. ")
+    print("Learning Kotlin")
+
+    println("")
+
+    print("Hi my name is ")
+    print("Nishant Kumar. \n")
+    print("Learning Kotlin")
+
+    println("")
+
+    print("Hi my name is ")
+    println("Nishant Kumar.")
+    print("Learning Kotlin")
+
+}
