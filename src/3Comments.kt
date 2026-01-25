98b2c83ee7e8e900d774a9c35a2635e62153305e
Nishant Kumar <nishant.kumaar002@gmail.com>
2026-01-25 16:38:44 +0530

    25-01-2026 04:38

---
+++ src/3Comments.kt
@@ -0,0 +1,9 @@
+fun main(){
+    // Single Line Comment. textual annotations within source code that are ignored by the compiler or interpreter
+
+    /*
+    Multi Line Comment.
+    textual annotations within source code
+    that are ignored by the compiler or interpreter
+     */
+}
