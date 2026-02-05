c6ad94cb0c491c819830d89935ee45be8d4bad0a
Nishant Kumar <nishant.kumaar002@gmail.com>
2026-02-05 19:02:38 +0530

    05-02-2026 07:02

--- src/7Nullability.kt
+++ src/7Nullability.kt
@@ -14,4 +14,18 @@ fun main(){
     }else{
         println("No Last Name")
     }
+
+    println("---Method 2: The Safe Call Operator ---")
+    val uppercaseLastName = lastName?.uppercase()
+    println(uppercaseLastName)
+
+    println("---Method 3: The Elvis Operator ---")
+    lastName = null
+    val uppercaseLastNameOrEmpty = lastName?.uppercase() ?: "No Last Name"
+    println(uppercaseLastNameOrEmpty)
+
+    println("---Method 4: The non null assertion (Not Safe)---")
+    lastName = "Kumar"
+    val upperCaseNullAssertion = lastName!!.uppercase()
+    println(upperCaseNullAssertion)
 }
